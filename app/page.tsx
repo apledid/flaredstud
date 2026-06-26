d363d54f2f7e0857ad5958924267a5705cc9346f
jvlian-ink <julianthomsbusiness@gmail.com>
2026-06-25 22:07:25 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-black">
       <h1 className="text-white text-4xl font-bold tracking-wide">
-        Maintenance underway
+        Maintenance underway Fuck somalian niggers
       </h1>
     </div>
   );
