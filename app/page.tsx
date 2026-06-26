df240a1149705734a6c00e1f4a2a811e725a2e96
jvlian-ink <julianthomsbusiness@gmail.com>
2026-06-25 22:21:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-black">
       <h1 className="text-white text-4xl font-bold tracking-wide">
-      i love u light
+      i love u mj
       </h1>
     </div>
   );
